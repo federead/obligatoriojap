0508baa2f6c5ff72386368ce0e2ddaace250f7a3
federead <fede.r.1794@gmail.com>
2022-09-16 23:08:30 -0300

    E3 - Rating OK

--- js/product-comment.js
+++ js/product-comment.js
@@ -1,6 +1,17 @@
 const commentCont = document.getElementById("commentContainer");
 let comments = [];
 
+function addRating(cantStars) { 
+    let stars = "";
+    for (let i=0; i<cantStars; i++) {
+        stars += `<span class="fa fa-star checked"></span>`;
+    }
+    for (let i=0; i<5-cantStars; i++) {
+        stars += `<span class="fa fa-star"></span>`;
+    }
+    return stars;
+}
+
 function loadComment() {
     let htmlContentToAppend = "";
       
@@ -8,7 +19,7 @@ function loadComment() {
         htmlContentToAppend = `        
         <div class="p-2 card">
             <div>
-                <p class="mb-1"><b>${comentarios.user}</b> - <small class="text-muted">${comentarios.dateTime}</small></p>
+                <p class="mb-1"><b>${comentarios.user}</b> - <small class="text-muted">${comentarios.dateTime}</small> - ${addRating(comentarios.score)}</p>
                 <p class="mb-1">${comentarios.description}</p>  
             </div>              
         </div>        
