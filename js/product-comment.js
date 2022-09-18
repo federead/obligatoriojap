c4eee9d556f67d85690673ee425dfe8b13ca893a
federead <fede.r.1794@gmail.com>
2022-09-17 22:03:33 -0300

    E3 completa

--- js/product-comment.js
+++ js/product-comment.js
@@ -1,12 +1,15 @@
 const commentCont = document.getElementById("commentContainer");
+const btnAddComment = document.getElementById("addComment");
+const productRatingComment = document.getElementById("productRating");
+const commentText = document.getElementById("commentText");
 let comments = [];
 
-function addRating(cantStars) { 
+function addRating(cantStars) {
     let stars = "";
-    for (let i=0; i<cantStars; i++) {
+    for (let i = 0; i < cantStars; i++) {
         stars += `<span class="fa fa-star checked"></span>`;
     }
-    for (let i=0; i<5-cantStars; i++) {
+    for (let i = 0; i < 5 - cantStars; i++) {
         stars += `<span class="fa fa-star"></span>`;
     }
     return stars;
@@ -14,7 +17,7 @@ function addRating(cantStars) {
 
 function loadComment() {
     let htmlContentToAppend = "";
-      
+
     for (const comentarios of comments) {
         htmlContentToAppend = `        
         <div class="p-2 card">
@@ -24,10 +27,38 @@ function loadComment() {
             </div>              
         </div>        
     `
-    commentCont.innerHTML += htmlContentToAppend;
+        commentCont.innerHTML += htmlContentToAppend;
     }
 }
 
+function fechaActual() {
+    const fecha = new Date();
+    const año = fecha.getFullYear();
+    const dia = fecha.getDate();
+    const mes = fecha.getMonth() + 1;
+    const hora = fecha.getHours();
+    const min = fecha.getMinutes();
+    const sec = fecha.getSeconds();
+    return `${año}-${mes}-${dia} ${hora}:${min}:${sec}`;
+}
+
+btnAddComment.addEventListener("click", () => {
+    let commentAdd = "";
+    if (commentText.value.trim()) {
+        commentAdd = `
+        <div class="p-2 card">
+            <div>
+                <p class="mb-1"><b>${sessionStorage.getItem("userName")}</b> - <small class="text-muted">${fechaActual()}</small> - ${addRating(productRatingComment.value)}</p>
+                <p class="mb-1">${commentText.value}</p>  
+            </div>              
+        </div>`
+        commentCont.innerHTML += commentAdd;
+    } else {
+        alert("Ingrese un comentario");
+    }
+    commentText.value = "";
+    productRatingComment.value = "1";
+});
 
 async function cargarDatos(url) {
     let response = await fetch(url);
