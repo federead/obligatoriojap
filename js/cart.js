e58ca72e07d0045439f66ab100c033c8f7ae1b77
federead <fede.r.1794@gmail.com>
2022-10-10 22:46:38 -0300

    E5 table in progress

--- js/cart.js
+++ js/cart.js
@@ -1,14 +1,29 @@
 let cartInfo = [];
-const cartContainer = document.getElementById("cartInfo");
+const cartTable = document.getElementById("cartProductsInfo");
 
+function loadCartContent() {
+    let htmlContentToAppend = "";
+
+    for (const cart of cartInfo) {
+        htmlContentToAppend = `
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>`
+        cartTable.innerHTML += htmlContentToAppend;
+    }
+}
 
 
 async function cargarDatos(url) {
     let response = await fetch(url);
     if (response.ok) {
         cartInfo = await response.json();
-        console.log(cartInfo)
-        
+        loadCartContent();        
     } else {
         alert("HTTP error: " + response.status);
     }
