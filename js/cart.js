269b97f9ad833394efb289db3097732783d90b72
federead <fede.r.1794@gmail.com>
2022-10-12 21:22:12 -0300

    E5 12-10

--- js/cart.js
+++ js/cart.js
@@ -4,12 +4,12 @@ const cartTable = document.getElementById("cartProductsInfo");
 function loadCartContent() {
     let htmlContentToAppend = "";
 
-    for (const cart of cartInfo) {
+    for (const cart of cartInfo.articles) {
         htmlContentToAppend = `
-        <tr>
-            <td></td>
-            <td></td>
-            <td></td>
+        <tr align="center">
+            <td><img src="${cart.image}" style="width:75%;" alt="${cart.name}"></td>
+            <td>${cart.name}</td>
+            <td>${cart.currency} ${cart.unitCost}</td>
             <td></td>
             <td></td>
             <td></td>
