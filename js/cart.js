b82910fd57731ff18660bcce9d385dfa2b153e49
federead <fede.r.1794@gmail.com>
2022-10-24 22:03:11 -0300

    Pasando autilizar local storage para el cart

--- js/cart.js
+++ js/cart.js
@@ -1,15 +1,24 @@
 let cartInfo = [];
+let cartInfoAPI = [];
 const cartTable = document.getElementById("cartProductsInfo");
 
 function precio(unidad, costUnitario, i){
     let resultado = unidad*costUnitario;  
-    if(resultado<0){
-        document.getElementById("cantProduct"+i).classList.add("is-invalid");     
+    if(resultado<1){
+        document.getElementById("cantProduct"+i).classList.add("invalidInput");        
 
     } else {
-        document.getElementById("subtotal").innerHTML = resultado;
+        document.getElementById("cantProduct"+i).classList.remove("invalidInput");
+        document.getElementById("subtotal"+i).innerHTML = resultado;
     }
 }
+
+// -- CARGANDO LOS DATOS DEL CART DESDE LA API --
+function loadCartContentFromAPI(){
+    
+}
+
+
 
 function loadCartContent() {
     let htmlContentToAppend = "";    
@@ -23,7 +32,7 @@ function loadCartContent() {
             <td>${cart.name}</td>
             <td>${cart.currency} ${cart.unitCost}</td>
             <td><input id="cantProduct${i}" min="0" class="form-control" style="width:50px;" oninput="precio(this.value,${cart.unitCost},${i})"></td>
-            <td>${cart.currency} <span id="subtotal"></span></td>
+            <td>${cart.currency} <span id="subtotal${i}"></span></td>
             <td></td>
         </tr>`
         cartTable.innerHTML += htmlContentToAppend;        
@@ -31,13 +40,13 @@ function loadCartContent() {
 }
 
 
-async function cargarDatos(url) {
+async function cargarDatosFromAPI(url) {
     let response = await fetch(url);
     if (response.ok) {
-        cartInfo = await response.json();
-        loadCartContent();        
+        cartInfoAPI = await response.json();
+        loadCartContentFromAPI();        
     } else {
         alert("HTTP error: " + response.status);
     }
 };
-cargarDatos(CART_INFO_URL + 25801 + ".json");
+cargarDatosFromAPI(CART_INFO_URL + 25801 + ".json");
