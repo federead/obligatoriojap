ca20408d5e6e7a750f7b5a69387757fe485f75b6
federead <fede.r.1794@gmail.com>
2022-10-26 17:56:21 -0300

    calculando costos

--- js/cart.js
+++ js/cart.js
@@ -1,4 +1,7 @@
 const cartTable = document.getElementById("cartProductsInfo");
+const envioPremium15 = document.getElementById("premiumEnvio");
+const envioExpress7 = document.getElementById("expressEnvio");
+const envioStandard5 = document.getElementById("standardEnvio");
 
 // Función que comprueba la cantidad de articulos y devuelve el subtotal
 function precio(unidad, costUnitario, i) {
@@ -10,8 +13,8 @@ function precio(unidad, costUnitario, i) {
         document.getElementById("cantProduct" + i).classList.remove("invalidInput");
         document.getElementById("subtotal" + i).innerHTML = resultado;
     }
+    console.log(document.getElementById("subtotal0").textContent);
 }
-
 
 // Carga y muestra los datos desde el Local Storage
 function loadCartContent() {
@@ -24,12 +27,12 @@ function loadCartContent() {
             <td><img src="${cart.image}" class="tdSize" alt="${cart.name}"></td>
             <td>${cart.name}</td>
             <td>${cart.currency} ${cart.unitCost}</td>
-            <td><input id="cantProduct${i}" min="0" class="form-control" style="width:50px;" oninput="precio(this.value,${cart.unitCost},${i})"></td>
+            <td><input id="cantProduct${i}" value="1" class="form-control" style="width:50px;" oninput="precio(this.value,${cart.unitCost},${i})"></td>
             <td>${cart.currency} <span id="subtotal${i}"></span></td>
             <td></td>
         </tr>`
         cartTable.innerHTML += htmlContentToAppend;
-    }
+    }    
 }
 
 loadCartContent();
