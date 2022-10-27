1f60d7c38424e3aad3a7eb417840ec6acc9169e2
federead <fede.r.1794@gmail.com>
2022-10-26 22:02:07 -0300

    tipo de cambio y eso

--- js/cart.js
+++ js/cart.js
@@ -4,16 +4,17 @@ const envioExpress7 = document.getElementById("expressEnvio");
 const envioStandard5 = document.getElementById("standardEnvio");
 
 // Función que comprueba la cantidad de articulos y devuelve el subtotal
-function precio(unidad, costUnitario, i) {
-    let resultado = unidad * costUnitario;
-    if (resultado < 1) {
-        document.getElementById("cantProduct" + i).classList.add("invalidInput");
+function precio(unidad, costUnitario, i, moneda) {
+    //falta comprobar el tipo de moneda y si es $ cambiarlo a U$S - probar con "operador ternario"
 
-    } else {
+    if (resultado < 1) {
+        
         document.getElementById("cantProduct" + i).classList.remove("invalidInput");
-        document.getElementById("subtotal" + i).innerHTML = resultado;
+        let resultado = unidad * costUnitario;        
+        document.getElementById("subtotal" + i).innerHTML = resultado;        
+    } else {
+        document.getElementById("cantProduct" + i).classList.add("invalidInput");        
     }
-    console.log(document.getElementById("subtotal0").textContent);
 }
 
 // Carga y muestra los datos desde el Local Storage
@@ -27,12 +28,27 @@ function loadCartContent() {
             <td><img src="${cart.image}" class="tdSize" alt="${cart.name}"></td>
             <td>${cart.name}</td>
             <td>${cart.currency} ${cart.unitCost}</td>
-            <td><input id="cantProduct${i}" value="1" class="form-control" style="width:50px;" oninput="precio(this.value,${cart.unitCost},${i})"></td>
-            <td>${cart.currency} <span id="subtotal${i}"></span></td>
+            <td><input type="number" id="cantProduct${i}" value="1" min="1" required class="form-control" style="width:63px;" oninput="precio(this.value,${cart.unitCost},${i},${cart.currency})"></td>
+            <td> USD <span id="subtotal${i}"></span></td>
             <td></td>
         </tr>`
         cartTable.innerHTML += htmlContentToAppend;
+        precio(1,cart.unitCost,i);
     }    
 }
 
+function calculateGeneralSubtotal() {
+    let cartInfo = JSON.parse(localStorage.getItem("productCart"));
+    let finalPrice = 0;
+    for(let i=0; i<cartInfo.length; i++){
+        let subtotalProd = parseInt(document.getElementById("subtotal"+i).textContent);
+        finalPrice = finalPrice + subtotalProd;      
+    }
+    return finalPrice;
+}
+
 loadCartContent();
+calculateGeneralSubtotal();
+/*envioPremium15.addEventListener("click", ()=> {
+    suma total x 15%
+});*/
