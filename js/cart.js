5c45713cbcc9d39edff8a33653d111b2874d3171
federead <fede.r.1794@gmail.com>
2022-10-27 12:00:18 -0300

    working on cart.js

--- js/cart.js
+++ js/cart.js
@@ -4,16 +4,22 @@ const envioExpress7 = document.getElementById("expressEnvio");
 const envioStandard5 = document.getElementById("standardEnvio");
 
 // Función que comprueba la cantidad de articulos y devuelve el subtotal
-function precio(unidad, costUnitario, i, moneda) {
-    //falta comprobar el tipo de moneda y si es $ cambiarlo a U$S - probar con "operador ternario"
+function precio(unidad, i) {
+    let cartInfo = JSON.parse(localStorage.getItem("productCart"));
+    let moneda = cartInfo[i].currency;
+    let unitCost = cartInfo[i].unitCost;
+    
+    console.log("el tipo de moneda es: " + moneda, typeof moneda);
+    console.log((moneda === "UYU") ? unitCost/40 : unitCost, moneda);
 
-    if (resultado < 1) {
-        
-        document.getElementById("cantProduct" + i).classList.remove("invalidInput");
-        let resultado = unidad * costUnitario;        
-        document.getElementById("subtotal" + i).innerHTML = resultado;        
+    let costoEnUsd = (moneda === "UYU") ? unitCost/40 : unitCost;    
+    let resultado = unidad * costoEnUsd;     
+
+    if (resultado < 0) {        
+        document.getElementById("cantProduct" + i).classList.add("invalidInput");
     } else {
-        document.getElementById("cantProduct" + i).classList.add("invalidInput");        
+        document.getElementById("cantProduct" + i).classList.remove("invalidInput");           
+        document.getElementById("subtotal" + i).innerHTML = resultado;
     }
 }
 
@@ -28,12 +34,12 @@ function loadCartContent() {
             <td><img src="${cart.image}" class="tdSize" alt="${cart.name}"></td>
             <td>${cart.name}</td>
             <td>${cart.currency} ${cart.unitCost}</td>
-            <td><input type="number" id="cantProduct${i}" value="1" min="1" required class="form-control" style="width:63px;" oninput="precio(this.value,${cart.unitCost},${i},${cart.currency})"></td>
+            <td><input type="number" id="cantProduct${i}" value="1" min="1" required class="form-control" style="width:63px;" oninput="precio(this.value, ${i})"></td>
             <td> USD <span id="subtotal${i}"></span></td>
             <td></td>
         </tr>`
         cartTable.innerHTML += htmlContentToAppend;
-        precio(1,cart.unitCost,i);
+        precio(1, i);
     }    
 }
 
@@ -44,6 +50,7 @@ function calculateGeneralSubtotal() {
         let subtotalProd = parseInt(document.getElementById("subtotal"+i).textContent);
         finalPrice = finalPrice + subtotalProd;      
     }
+    console.log("total de toda la compra: " + finalPrice);
     return finalPrice;
 }
 
