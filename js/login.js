5b4260d983029797436057f9323165eca185a9f6
federead <fede.r.1794@gmail.com>
2022-11-02 22:40:07 -0300

    Starting E7

--- js/login.js
+++ js/login.js
@@ -1,26 +1,21 @@
 const userLogin = document.getElementById("user-login");
-const passwordLogin = document.getElementById("password-login");
-const singInBtn = document.getElementById("sing-in-btn");
 
-/*function onSignIn(googleUser) {
-    var profile = googleUser.getBasicProfile();
-    console.log("ID: " + profile.getId());
-    console.log('Full Name: ' + profile.getName());
-    console.log('Given Name: ' + profile.getGivenName());
-    console.log('Family Name: ' + profile.getFamilyName());
-    console.log("Image URL: " + profile.getImageUrl());
-    console.log("Email: " + profile.getEmail());
-}*/
-
-function checkLogIn() {
-    if (userLogin.value.trim().length > 0 && passwordLogin.value.trim().length > 0){
-        localStorage.setItem("userName", userLogin.value);
-        window.location = "index.html";        
-    } else {
-        alert ("Los campos no pueden estar vacios");
-    }
-}
-
-singInBtn.addEventListener("click", () => {
-    checkLogIn();
-  });
+//Funci{on de Bootstrap que valida el login y redirige a index si todo esta ok
+(function () {
+    var forms = document.querySelectorAll('.needs-validation');
+    Array.prototype.slice.call(forms)
+        .forEach(function (form) {
+            form.addEventListener('submit', function (event) {                
+                if (!form.checkValidity()) {
+                    event.preventDefault()
+                    event.stopPropagation()
+                } else {
+                    event.stopPropagation();
+                    event.preventDefault();
+                    localStorage.setItem("userName", userLogin.value);
+                    location.assign("index.html");
+                }
+                form.classList.add('was-validated')
+            }, false)
+        })
+})()
