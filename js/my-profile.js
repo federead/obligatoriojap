eff659c01894a872426784617f4bd3a21cb73b43
federead <fede.r.1794@gmail.com>
2022-11-06 20:13:07 -0300

    E7 check email

---
+++ js/my-profile.js
@@ -0,0 +1,34 @@
+const profileMail = document.getElementById("profileMail");
+
+
+if (!localStorage.getItem("userProfile")) {
+    profileMail.value = localStorage.getItem("userName");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+(function () {
+    'use strict'
+    var forms = document.querySelectorAll('.needs-validation')
+
+    Array.prototype.slice.call(forms)
+        .forEach(function (form) {
+            form.addEventListener('submit', function (event) {
+                if (!form.checkValidity()) {
+                    event.preventDefault()
+                    event.stopPropagation()
+                }
+
+                form.classList.add('was-validated')
+            }, false)
+        })
+})()
